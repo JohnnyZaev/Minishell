f47284d6bf0a9a6268dfe35b3bf30b79d7bee38d
johnnyzaev <evgenijantonov801@gmail.com>
2022-02-08 10:48:03 +0300

    norm fix for main

--- src/main.c
+++ src/main.c
@@ -6,20 +6,23 @@
 /*   By: gvarys <gvarys@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 16:38:16 by gvarys            #+#    #+#             */
-/*   Updated: 2022/02/07 16:41:30 by gvarys           ###   ########.fr       */
+/*   Updated: 2022/02/08 10:47:37 by gvarys           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(void)
+int	main(int argc, char **argv, char **envp)
 {
-    char *str;
-    while (1)
-    {
-        str = readline("minishell $ ");
-        //execute_process(str, envp);
-        free(str);
-    }
+	char	*str;
+
+	(void) argc;
+	(void) argv;
+	while (true)
+	{
+		str = readline("minishell $ ");
+		//execute_process(str, envp);
+		free(str);
+	}
 	return (0);
 }
