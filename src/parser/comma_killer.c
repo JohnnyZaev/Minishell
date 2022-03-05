2675340524284968a3fef6c2476d564e61ac1d12
Estela Reginia <ereginia@oa-d5.msk.21-school.ru>
2022-03-05 15:36:32 +0300

    norma na zhene

--- src/parser/comma_killer.c
+++ src/parser/comma_killer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   comma_killer.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gvarys <gvarys@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: ereginia <ereginia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 11:20:45 by ereginia          #+#    #+#             */
-/*   Updated: 2022/03/05 15:13:56 by gvarys           ###   ########.fr       */
+/*   Updated: 2022/03/05 15:34:23 by ereginia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,66 +40,59 @@ static int	count_words(char *str)
 
 static int	char_count(char *str, int *i)
 {
-	int	j;
-	int	k;
-	int	flag;
+	t_stuff	iter;
 
-	j = 0;
-	k = *i;
-	flag = 0;
+	ft_memset(&iter, 0, sizeof(iter));
+	iter.k = *i;
 	while (str[*i])
 	{
-		if (str[*i] == '\'' && flag == 0)
+		if (str[*i] == '\'' && iter.flag == 0)
 		{
-			j++;
-			flag = 1;
+			iter.j++;
+			iter.flag = 1;
 		}
-		else if (str[*i] == '\'' && flag == 1)
-			flag = 0;
-		if (str[*i] == '\"' && flag == 0)
+		else if (str[*i] == '\'' && iter.flag == 1)
+			iter.flag = 0;
+		if (str[*i] == '\"' && iter.flag == 0)
 		{
-			j++;
-			flag = 2;
+			iter.j++;
+			iter.flag = 2;
 		}
-		else if (str[*i] == '\"' && flag == 2)
-			flag = 0;
-		if (str[*i] == ' ' && !flag)
+		else if (str[*i] == '\"' && iter.flag == 2)
+			iter.flag = 0;
+		if (str[*i] == ' ' && !iter.flag)
 			break ;
 		*i = *i + 1;
 	}
-	j = *i - 2 * j - k;
-	return (j);
+	return (*i - 2 * iter.j - iter.k);
 }
 
 static void	write_word(char *dest, char *from, int size)
 {
-	int	i;
-	int	j;
-	int	flag;
+	t_stuff	iter;
 
-	i = 0;
-	j = 0;
-	flag = 0;
-	while (from[i] && j < size)
+	ft_memset(&iter, 0, sizeof(iter));
+	while (from[iter.i] && iter.j < size)
 	{
-		if (from[i] == '\'' && flag == 0)
-			flag = 1;
-		else if (from[i] == '\'' && flag == 1)
-			flag = 0;
-		if (from[i] == '\"' && flag == 0)
-			flag = 2;
-		else if (from[i] == '\"' && flag == 2)
-			flag = 0;
-		if ((from[i] == '\'' && flag != 2) || (from[i] == '\"' && flag != 1))
+		if (from[iter.i] == '\'' && iter.flag == 0)
+			iter.flag = 1;
+		else if (from[iter.i] == '\'' && iter.flag == 1)
+			iter.flag = 0;
+		if (from[iter.i] == '\"' && iter.flag == 0)
+			iter.flag = 2;
+		else if (from[iter.i] == '\"' && iter.flag == 2)
+			iter.flag = 0;
+		if ((from[iter.i] == '\'' && iter.flag != 2)
+			|| (from[iter.i] == '\"' && iter.flag != 1))
 		{
-			i++;
+			iter.i++;
 			continue ;
 		}
-		dest[j] = from[i];
-		i++;
-		j++;
+		dest[iter.j] = from[iter.i];
+		iter.i++;
+		iter.j++;
 	}
-	dest[j] = '\0';
+	dest[iter.j] = '\0';
 }
 
 static void	write_split(char **split, char *str)
