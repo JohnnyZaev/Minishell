d9e7884aa7a5e7f04f40afd8dec91ce41e8cd199
Estela Reginia <ereginia@oa-d5.msk.21-school.ru>
2022-03-05 13:06:05 +0300

    quotes in quotes

--- src/parser/inseption_split.c
+++ src/parser/inseption_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   inseption_split.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gvarys <gvarys@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: ereginia <ereginia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 10:52:41 by ereginia          #+#    #+#             */
-/*   Updated: 2022/03/04 16:10:38 by gvarys           ###   ########.fr       */
+/*   Updated: 2022/03/05 13:05:00 by ereginia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,63 @@
 
 static int is_dollar(char *str)
 {
-    int i = 0;
-    int flag = 0;
+	int i;
+	int flag;
 
-    while (str[i])
-    {
-        if (str[i] == '\'')
-            flag = !flag;
-        if (str[i] == '$' && !flag)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	flag = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' && flag == 0)
+			flag = 1;
+		else if (str[i] == '\'' && flag == 1)
+			flag = 0;
+		if (str[i] == '\"' && flag == 0)
+			flag = 2;
+		else if (str[i] == '\"' && flag == 2)
+			flag = 0;
+		if (str[i] == '$' && flag != 1)
+			return (1);
+		i++;
+	}
+	return (0);
 }
-//////////////////////////////////////////////////////
+
 static int dollar_len(t_minishell *m_shell, char *str, int *l)
 {
-    int i = 0;
-    int k = 0;
-    char *buf;
+	int i;
+	int k;
+	char *buf;
 
-    
-    while (str[i + 1] && (ft_isalnum(str[i + 1]) || str[i + 1] == '?'))
-        i++;
-    *l = *l + i + 1;
-    buf = ft_substr(str, 1, i);
-    if (!ft_strncmp(buf, "?", 2))
-    {
-        k++;
-        while (i / 10)
-        {
-            i = i / 10;
-            k++;
-        }
-    }
-    else
-        k = ft_strlen(search_envs(&m_shell->envs, buf));
-    free(buf);
-    return (k);
+	i = 0;
+	k = 0;
+	while (str[i + 1] && (ft_isalnum(str[i + 1]) || str[i + 1] == '?'))
+		i++;
+	*l = *l + i + 1;
+	buf = ft_substr(str, 1, i);
+	if (!ft_strncmp(buf, "?", 2))
+	{
+		k++;
+		while (i / 10)
+		{
+			i = i / 10;
+			k++;
+		}
+	}
+	else
+		k = ft_strlen(search_envs(&m_shell->envs, buf));
+	free(buf);
+	return (k);
 }
 
 static int word_len(t_minishell *m_shell, char *str)
 {
-    int i = 0;
-    int k = 0;
-    int flag = 0;
+	int i = 0;
+	int k = 0;
+	int flag = 0;
 
-    while (str[i])
-    {
+	while (str[i])
+	{
 		if (str[i] == '\'' && flag == 0)
 		{
 			flag = 1;
@@ -97,80 +106,80 @@ static int word_len(t_minishell *m_shell, char *str)
 		}
 		if (str[i] && !flag)
 		{
-		    if (str[i] == '$')
-		        k = k + dollar_len(m_shell, &str[i], &i);
-            else
-            {
-                k++;
-		        i++;
-            }
+			if (str[i] == '$')
+				k = k + dollar_len(m_shell, &str[i], &i);
+			else
+			{
+				k++;
+				i++;
+			}
 		}
-    }
-    return (k);
+	}
+	return (k);
 }
-//////////////////////////////////////////////////////
 
 static char *word_replace(t_minishell *m_shell, char *str)
 {
-    char    *res;
-    char    *buf;
-    char    *temp;
-    int     i = 0;
-    int     j = 0;
-    int     k = 0;
-    int     flag = 0;
+	char	*res;
+	char	*buf;
+	char	*temp;
+	int		i = 0;
+	int		j = 0;
+	int		k = 0;
+	int		flag = 0;
 
-    res = (char *)malloc(sizeof(char) * word_len(m_shell, str) + 1);
-    while (str[i])
-    {
-        if (str[i] == '\'' && flag == 0)
+	res = (char *)malloc(sizeof(char) * word_len(m_shell, str) + 1);
+	while (str[i])
+	{
+		if (str[i] == '\'' && flag == 0)
 			flag = 1;
 		else if (str[i] == '\'' && flag == 1)
-		    flag = 0;
+			flag = 0;
 		if (str[i] == '\"' && flag == 0)
 			flag = 2;
 		else if (str[i] == '\"' && flag == 2)
-		    flag = 0;
-        if (str[i] != '$' || flag == 1)
-            res[k] = str[i];
-        else
-        {
-            while (str[i + j + 1] && (ft_isalnum(str[i + j + 1]) || str[i + j + 1] == '?'))
-                j++;
-            buf = ft_substr(&str[i], 1, j);
-            if (!ft_strncmp(buf, "?", 2))
-                temp = ft_itoa(m_shell->error_code);
-            else
-                temp = search_envs(&m_shell->envs, buf);
-            ft_strlcpy(&res[k], temp, ft_strlen(temp) + 1);
-            k = k + ft_strlen(temp);
+			flag = 0;
+		if (str[i] != '$' || flag == 1)
+			res[k] = str[i];
+		else
+		{
+			while (str[i + j + 1] && (ft_isalnum(str[i + j + 1]) || str[i + j + 1] == '?'))
+				j++;
+			buf = ft_substr(&str[i], 1, j);
+			if (!ft_strncmp(buf, "?", 2))
+				temp = ft_itoa(m_shell->error_code);
+			else
+				temp = search_envs(&m_shell->envs, buf);
+			ft_strlcpy(&res[k], temp, ft_strlen(temp) + 1);
+			k = k + ft_strlen(temp);
 			if (!search_envs(&m_shell->envs, buf))
 				free(temp);
-            free(buf);
-            i = i + j + 1;
-            j = 0;
-            continue ;
-        }
-        i++;
-        k++;
-    }
-    res[k] = '\0';
-    return res;
+			free(buf);
+			i = i + j + 1;
+			j = 0;
+			continue ;
+		}
+		i++;
+		k++;
+	}
+	res[k] = '\0';
+	return (res);
 }
 
 void split_inseption(t_minishell *m_shell, char **spt)
 {
-    int i = 0;
-    char *buf;
-    
-    while (spt[i])
-    {
-        if (is_dollar(spt[i]))
-        {
-            buf = spt[i];
-            spt[i] = word_replace(m_shell, buf);
-            free(buf);
-        }
-        i++;
-    }
+	int		i;
+	char	*buf;
+
+	i = 0;
+	while (spt[i])
+	{
+		if (is_dollar(spt[i]))
+		{
+			buf = spt[i];
+			spt[i] = word_replace(m_shell, buf);
+			free(buf);
+		}
+		i++;
+	}
 }
